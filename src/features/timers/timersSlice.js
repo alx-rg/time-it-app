44d9960de397420ca0e27a6162e689a027b177b8
Alex Ross <alxrg@me.com>
2023-03-29 14:41:39 -0500

    added Timer actions

--- src/features/timers/timersSlice.js
+++ src/features/timers/timersSlice.js
@@ -9,10 +9,16 @@ export const timersSlice = createSlice({
   name: "timers",
   initialState,
   reducers: {
-
-  },
+    addTimer: (state, action) => {
+      state.value.push(new Timer(action.payload.name))
+    },
+    toggleTimer: (state, action) => {
+      state.value[action.payload].isRunning =
+      !state.value[action.payload].isRunning
+    }
+  }
 })
 
-export const { } = timersSlice.actions;
+export const { addTimer, toggleTimer } = timersSlice.actions;
 
 export default timersSlice.reducer;
