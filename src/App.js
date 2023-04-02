c7952174813fe600ff5e73193b5b08105b77aad1
Alex Ross <alxrg@me.com>
2023-04-01 19:19:33 -0500

    added net timer input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import NewTimer from './components/NewTimer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Timer List</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter your timer's <code>name</code> and click save to start it.
         </p>
-        <a
+        <NewTimer />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
